babb20a812b323d645f9c5b5081287c1701d8b4e
Mehroz Ali <dev.mehrozali@gmail.com>
2023-12-16 20:38:24 +0500

    Add Clerk auth

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,12 @@
+import { PropsWithChildren } from 'react';
+import { ClerkProvider } from "@clerk/nextjs";
+
+const PlatformLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <ClerkProvider>
+      {children}
+    </ClerkProvider>
+  )
+}
+
+export default PlatformLayout;
